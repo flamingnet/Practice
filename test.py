01f68eb80e36e3f1dd1d0eea512c1bb3f1deb9ec
Chet Murthy <flamingnet@gmail.com>
2015-12-01 22:07:03 -0600

    Changed Sad to Happy

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import urllib2
 response = urllib2.urlopen('http://gutenberg.org/files/135/135-h/135-h.htm#link2H_4_0347/')
 html = response.read()
 
-sad = 0
+happy = 0
 
 list_of_words = html.split(' ')
 
 for word in list_of_words:
 	if word == 'Les Miserables':
-		sad += 1
+		happy += 1
 
-print sad
+print happy
